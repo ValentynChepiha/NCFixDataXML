1e1b92f3f90d1928ee0de71062c70fac3d9ed1a5
Valentyn Chepiha <valentin.chepiga@gmail.com>
2022-01-20 21:26:21 +0200

    Init project

---
+++ src/test/java/ua/edu/sumdu/j2se/chepiha/fixer/MainTest.java
@@ -0,0 +1,20 @@
+package ua.edu.sumdu.j2se.chepiha.fixer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
